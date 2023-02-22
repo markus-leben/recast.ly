f57b2f781897ae7494c2d662b19de40f5047ad5a
Markus Leben <markus_leben@mac.com>
2023-02-22 09:11:50 -0800

    finished everything before the API stuff

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,20 +1,32 @@
-var App = () => (
-  <div>
-    <nav className="navbar">
-      <div className="col-md-6 offset-md-3">
-        <div><h5><em>search</em> view goes here</h5></div>
+import Search from './Search.js';
+import VideoPlayer from './VideoPlayer.js';
+import VideoList from './VideoList.js';
+import exampleVideoData from '../data/exampleVideoData.js';
+const { useState } = React;
+
+
+
+var App = () => {
+  const [videoList, setVideoList] = useState(exampleVideoData);
+  const [currentVideo, setCurrentVideo] = useState(exampleVideoData[0]);
+  const changeVideo = (newVideo) => (setCurrentVideo(newVideo));
+  return (
+    <div>
+      <nav className="navbar">
+        <div className="col-md-6 offset-md-3">
+          <Search/>
+        </div>
+      </nav>
+      <div className="row">
+        <div className="col-md-7">
+          <VideoPlayer video={currentVideo}/>
+        </div>
+        <div className="col-md-5">
+          <VideoList videos={videoList} changeVideo={changeVideo}/>
+        </div>
       </div>
-    </nav>
-    <div className="row">
-      <div className="col-md-7">
-        <div><h5><em>videoPlayer</em> view goes here</h5></div>
-      </div>
-      <div className="col-md-5">
-        <div><h5><em>videoList</em> view goes here</h5></div>
-      </div>
-    </div>
-  </div>
-);
+    </div> );
+};
 
 // In the ES6 spec, files are "modules" and do not share a top-level scope
 // `var` declarations will only exist globally where explicitly defined
--- src/lib/searchYouTube.js
+++ src/lib/searchYouTube.js
@@ -1,11 +1,36 @@
 import { API_KEY, YOUTUBE_API_KEY } from '../config/config.js';
 
+var server = 'https://app-hrsei-api.herokuapp.com/api/recastly/videos';
+
 $.ajaxPrefilter(function (settings, _, jqXHR) {
   jqXHR.setRequestHeader('Authorization', API_KEY);
 });
 
 var searchYouTube = (query, callback) => {
   // TODO
+  $.ajax({
+    url: server,
+    type: 'GET',
+    contentType: 'application/json',
+    success: callback,
+    error: function(error) {
+      console.error('Recastly YouTube Server: Failed to fetch video', error);
+    }
+  });
 };
 
 export default searchYouTube;
+
+
+// readAll: function(successCB, errorCB = null) {
+//   $.ajax({
+//     url: Parse.server,
+//     type: 'GET',
+//     data: { order: '-createdAt' },
+//     contentType: 'application/json',
+//     success: successCB,
+//     error: errorCB || function(error) {
+//       console.error('chatterbox: Failed to fetch messages', error);
+//     }
+//   });
+// }
